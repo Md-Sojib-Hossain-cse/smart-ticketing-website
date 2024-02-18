290a46d3553432f1524665aaba3ed9ed6b631efb
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-02-18 23:02:31 +0600

    remaining seat value dynamic

--- scripts/index.js
+++ scripts/index.js
@@ -3,5 +3,13 @@ const seats = document.querySelectorAll('.seats');
 for(const seat of seats){
     seat.addEventListener('click' , function(e){
         const pressedSeat = e.target.innerText;
-    })
+        // seat color & bg color changed 
+        addColorById(pressedSeat);
+        //remaining seat calculate
+        const remainingSeatValue = getInnerValueById('remainingSeatCount');
+        const remainingSeat = remainingSeatValue - 1;
+        setInnerValueById('remainingSeatCount' , remainingSeat);
+
+        console.log(remainingSeat);
+    });
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,1 +1,19 @@
-function changeColorById(){}
+// change color and bg color by id 
+function addColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.style.color = 'white';
+    element.style.backgroundColor ='#1DD100';
+}
+
+// get element by Id
+function getInnerValueById (elementId){
+    const element = document.getElementById(elementId);
+    const elementText = element.innerText;
+    const elementValue = parseInt(elementText);
+    return elementValue;
+}
+//set element value by id
+function setInnerValueById (elementId , value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
