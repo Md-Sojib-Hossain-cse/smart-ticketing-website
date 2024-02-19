3bdd7ca3f9a1587bc9534d65d75d2ec27f0b0df5
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-02-19 11:15:42 +0600

    maximum seat selection function added

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 const seats = document.querySelectorAll('.seats');
 const ticketPrice = ticketPriceById('ticketPrice');
 const nextButton = document.getElementById('nextButton');
+const couponInput = document.getElementById('couponInput');
+const couponClaimButton = document.getElementById('couponClaimButton');
 let countValue = 0;
 
 userNumber.addEventListener('input', function (e) {
@@ -21,6 +23,12 @@ userNumber.addEventListener('input', function (e) {
 for (const seat of seats) {
     seat.addEventListener('click', function (e) {
         const pressedSeat = e.target.innerText;
+        const pressedSeatButton = document.getElementById(pressedSeat);
+        //maximum 4 sets can be selected
+        if (countValue >= 4) {
+            alert('You have selected maximum number of seats !');
+            return;
+        }
         // seat color & bg color changed 
         addColorById(pressedSeat);
         //remaining seat calculate
@@ -45,6 +53,15 @@ for (const seat of seats) {
             const nextButton = document.getElementById('nextButton');
             nextButton.removeAttribute("disabled");
             nextButton.style.backgroundColor = '#1DD100';
+        }
+        //disabled selected seat after selected
+        pressedSeatButton.setAttribute('disabled' , true)
+        //after selecting 4 seats coupon field will be enabled
+        if (countValue >= 4) {
+            couponInput.removeAttribute("disabled");
+            couponClaimButton.removeAttribute("disabled");
+            couponClaimButton.style.backgroundColor = '#1DD100';
+            return;
         }
     });
 }
